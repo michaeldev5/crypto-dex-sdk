7e373729b7d3d6a8b5aa1fcf0dba973226cbf3d3
Rikkard29 <absolon.mm@gmail.com>
2023-12-19 16:10:34 +0100

    Add build command to bifrost package

--- packages/parachains-impl/bifrost/systems/Checker/types.ts
+++
@@ -1,6 +0,0 @@
-import type { ButtonProps } from '@crypto-dex-sdk/ui'
-import type { ReactNode } from 'react'
-
-export interface CheckerButton extends ButtonProps<'button'> {
-  children: ReactNode
-}
